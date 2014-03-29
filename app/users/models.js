8e9d070a1ad883cc638e81ccb8f3ea99d78f708d
Joseph Núñez <toctochello@gmail.com>
2014-03-29 03:50:24 -0430

    Simplifying the naming conventions in /users/models.js

--- app/users/models.js
+++ app/users/models.js
@@ -1,15 +1,15 @@
 
-var mongoose = require('mongoose')
+var db = require('mongoose')
   , crypto = require('crypto');
 
-var UserSchema = new(mongoose.Schema)({
+var Schema = new(db.Schema)({
   name: {type: String, trim: true, default: ''},
   email: {type: String, lowercase: true, trim: true, default: ''},
   hash: {type: String, default: ''},
   salt: {type: String, default: ''},
 });
 
-UserSchema
+Schema
   .virtual('password')
   .set(function(password) {
     this._password = password;
@@ -20,7 +20,7 @@ UserSchema
       return this._password;
   });
 
-UserSchema.methods = {
+Schema.methods = {
   authenticate: function(password) {
     return this.genHash(password) == this.hash;
   },
@@ -34,8 +34,8 @@ UserSchema.methods = {
   },
 };
 
-var User = mongoose.model('User', UserSchema);
-UserSchema.path('email').validate(function(email, fn) {
+var User = db.model('User', Schema);
+Schema.path('email').validate(function(email, fn) {
   if (this.isNew || this.isModified('email')) {
     User.find({ email: email }).exec(function(err, users) {
       fn(!err && users.length == 0);
