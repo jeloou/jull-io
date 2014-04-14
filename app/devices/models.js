30542e9294066b3b3f3400b87d39199eb9434f09
Joseph Núñez <toctochello@gmail.com>
2014-04-14 12:08:45 -0430

    Moving the keys generation code to the devices

--- app/devices/models.js
+++ app/devices/models.js
@@ -1,46 +1,55 @@
-var db = require('mongoose');
+var db = require('mongoose')
+  , uuid = require('node-uuid');
 
 var ObjectId = db.Schema.ObjectId;
 var Schema = new(db.Schema)({
   name: {type: String, trim: true, required: true},
   description: {type: String, trim: true, required: true},
   user: {type: ObjectId, ref: 'User', required: true},
-  key: {type: ObjectId, ref: 'Key'},
+  key: db.Schema.Types.Mixed,
 });
 
 var Device = db.model('Device', Schema);
 Schema.pre('save', function(next) {
-  var key, that = this;
+  var genCredentials, key, token, that = this;
   
   if (!this.isNew) {
     return next();
   }
   
-  key = new(db.model('Key'));
-  key.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    that.key = key;
-    next();
-  });
+  (genCredentials = function() {
+    key = uuid.v1();
+    token = genToken();
+    
+    Device.find(
+      {'key.key': key, 'key.token': token}, function(err, devices) {
+	if (err) {
+	  next(err);
+	  return;
+	}
+
+	if (devices.length > 0) {
+	  genCredentials();
+	  return;
+	}
+
+	that.key = {key: key, token: token};
+	next();
+      }
+    );
+  })();
 });
 
 Schema.post('save', function() {
   var User = db.model('User'), that = this;
   
-  this.populate('key', function(err) {
-    if (err) {
-      throw err;
+  User.update(
+    {_id: this.user}, {$addToSet: {devices: this.key.key}}, function(err) {
+      if (err) {
+	next(err);
+      }
     }
-    
-    User.update(
-      {_id: that.user}, {$addToSet: {devices: that.key.key}}, function(err) {
-	if (err) {
-	  throw err
-	}
-      });
-  });
+  );
 });
 
 Schema.path('name').validate(function(name, fn) {
@@ -52,3 +61,9 @@ Schema.path('name').validate(function(name, fn) {
     fn(true);
 }, 'Duplicated device name');
 
+function genToken() {
+  var rand = function() {
+    return Math.random().toString(36).substr(2); 
+  };
+  return rand() + rand();
+}
