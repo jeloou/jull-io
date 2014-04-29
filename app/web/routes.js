b74d85faf8d7f525c223477eeb4508a7ce4e260b
Joseph Núñez <toctochello@gmail.com>
2014-04-28 22:13:05 -0430

    Temporal landing structure, almost all is missing

--- app/web/routes.js
+++ app/web/routes.js
@@ -3,7 +3,9 @@ var auth = require('../middlewares/authorization');
 module.exports = (function(app) {
   app.get('/', function(req, res) {
     if (!req.user) {
-      res.render('landing');
+      res.render('landing', {
+	title: 'jull.io – m2m instant messaging for moving things'
+      });
       return;
     }
     
