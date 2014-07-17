ce99b0278bda8b8f348cf42e41a3e1aea479b475
Joseph Núñez <toctochello@gmail.com>
2014-07-17 16:50:50 -0430

    Cleaning up the configuration file

--- config.js
+++ config.js
@@ -7,53 +7,129 @@ var express = require('express')
   , socket = require('./lib/socket')
   , store = require('./lib/store')
   , path = require('path')
+  , util = require('util')
   , fs = require('fs');
 
-var EXPRESS_SID_KEY = '_jull.io_sess'
-  , COOKIE_SECRET = 'very secret string';
-
-var cookieParser = express.cookieParser(COOKIE_SECRET)
-  , sessionStore;
-
-sessionStore = new(mongoStore)({
-  url: 'mongodb://localhost/dev',
-  collection : 'sessions'
-});
-
 module.exports = exports = function(app, io, passport) {
-  var User, connect, app_path;
+  /*
+    MongoDb configuration
+  */
+  var DATABASE_HOST
+    , DATABASE_NAME
+    , DATABASE_USER
+    , DATABASE_PASSWORD;
 
-  connect = function() {
-    var options = { server: { socketOptions: { keepAlive: 1 } } }
-    db.connect('mongodb://localhost/dev', options);
+  switch (app.settings.env) {
+    case 'production':
+      DATABASE_HOST = process.env.DATABASE_HOST;
+      DATABASE_NAME = process.env.DATABASE_NAME;
+      DATABASE_USER = process.env.DATABASE_USER;
+      DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+      break;
+    
+    case 'development':
+      DATABASE_HOST = 'localhost';
+      DATABASE_NAME = 'jull-io-db';
+      break;
+    
+    default:
+      break;
+  }
+  
+  var DATABASE_URL = util.format(
+    'mongodb://%s/%s', DATABASE_HOST, DATABASE_NAME);
+  
+  var connect = function() {
+    var options = {
+      server: { 
+	socketOptions: { 
+	  keepAlive: 1 
+	} 
+      },
+    };
+    
+    if (DATABASE_USER && DATABASE_PASSWORD) {
+      options.user = DATABASE_USER;
+      options.password = DATABASE_PASSWORD;
+    }
+    
+    db.connect(DATABASE_URL, options);
   };
+  
   connect();
-
-  // Error handler
-  db.connection.on('error', function (err) {
+  
+  db.connection.on('error', function(err) {
     console.log(err);
   });
-
-  // Reconnect when closed
-  db.connection.on('disconnected', function () {
+  
+  db.connection.on('disconnected', function() {
     connect();
   });
+  
+  /*
+    Sessions configuration
+  */
+  var SESSION_ID_KEY
+    , COOKIE_SECRET;
+
+  switch(app.settings.env) {
+    case 'production':
+      SESSION_ID_KEY = process.env.SESSION_ID_KEY;
+      COOKIE_SECRET = process.env.COOKIE_SECRET;
+      break;
     
-  app_path = path.resolve('./app/');
-  fs.readdirSync(app_path).forEach(function(dir) {
-    var module = path.join(app_path, dir, 'models.js');
+    case 'development':
+      SESSION_ID_KEY = '_jull.io'
+      COOKIE_SECRET = 'very secret string';
+      break;
+
+    default:
+      break;
+  }
+  
+  var cookieParser = express.cookieParser(COOKIE_SECRET)
+    , sessionStore;
+  
+  sessionStore = new(mongoStore)({
+    url: DATABASE_URL,
+    collection: 'sessions'
+  });
+  
+  /* 
+     .bodyParser should be above .methodOverride
+  */
+  app.use(cookieParser);
+  app.use(express.bodyParser())
+  app.use(express.methodOverride());
+  app.use(express.session({
+    key: SESSION_ID_KEY,
+    store: sessionStore,
+    cookie: {
+      httpOnly: true
+    },
+  }));
+  
+  /*
+    Loading models
+  */
+  
+  var apps = path.resolve('./app/');
+  fs.readdirSync(apps).forEach(function(app) {
+    var models = path.join(apps, app, 'models.js')
     
-    if (fs.existsSync(module)) {
-      require(module);
+    if (fs.existsSync(models)) {
+      require(models);
     }
   });
 
-  User = db.model('User');
-  
+  /*
+    Configuring passport
+  */
+  var User = db.model('User');
   passport.serializeUser(function(user, fn) {
     fn(null, user.id);
   });
-
+  
   passport.deserializeUser(function(id, fn) {
     User.findOne({_id: id}, function(err, user) {
       fn(err, user);
@@ -67,86 +143,78 @@ module.exports = exports = function(app, io, passport) {
   function(email, password, fn) {
     User.findOne({email: email}, function(err, user) {
       if (err) {
-        return fn(err);
+        fn(err);
+	return;
       }
 	
       if (!user || !user.authenticate(password)) {
-        return fn(null, false, {message: 'email or password incorrect'});
+        fn(null, false, {
+	  message: 'email or password incorrect'
+	});
+	return;
       }
 	
-      return fn(null, user);
+      fn(null, user);
     });
   }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
   
+  /*
+    Adding favicon and serving compressed static files
+  */
+  app.use(express.favicon());
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.compress({
     filter: function (req, res) {
       return /json|text|javascript|css/.test(res.getHeader('Content-Type'))
     },
     level: 9
   }));
-
-  app.use(express.favicon());
-  /*
-    I'll solve this later
-    app.use(express.static(config.root + '/public'));
-  */
-
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade')
-
-  app.configure(function () {
-    // cookieParser should be above session
-    app.use(cookieParser);
-      
-    // bodyParser should be above methodOverride
-    app.use(express.bodyParser())
-    app.use(express.methodOverride());
-
-    // express/mongo session storage
-    app.use(express.session({
-      key: EXPRESS_SID_KEY,
-      store: sessionStore,
-      cookie: {
-	httpOnly: true
-      },
-    }));
-  });
   
-  app.use(passport.initialize());
-  app.use(passport.session());
+  /*
+    Configuring views dir and engine
+  */
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade')
   
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
   
-  app.use(function(err, req, res, next) {
-    // treat as 404
-    if (err.message
-        && (~err.message.indexOf('not found')
-	    || (~err.message.indexOf('Cast to ObjectId failed')))) {
-      return next();
-    }
-    
-    // log it
-    // send emails if you want
-    console.error(err.stack)
-    
-    // error page
-    res.status(500).render('500', { 
-      error: err.stack 
-    });
-  });
+  /*
+    Redis configuration, used by ascoltatori, socket.io and koa.
+  */
+  var REDIS_HOST
+    , REDIS_PORT;
   
-  io = new(SessionSockets)(io, sessionStore, cookieParser, EXPRESS_SID_KEY);
-  var settings = {
+  switch(app.settings.env) {
+    case 'production':
+      REDIS_HOST = process.env.REDIS_HOST;
+      REDIS_PORT = process.env.REDIS_PORT;
+      break;
+    
+    case 'development':
+      REDIS_HOST = 'localhost';
+      REDIS_PORT = 6379;
+      break;
+    
+    default:
+      break;
+  }
+  
+  var redisSettings = {
     type: 'redis',
-    host: 'localhost',
-    port: 6379,
+    host: REDIS_HOST,
+    port: REDIS_PORT
   };
   
-  socket(io, settings);
-  store(settings);
+  io = new(SessionSockets)(io, sessionStore, cookieParser, SESSION_ID_KEY)
+  
+  socket(io, redisSettings);
+  store(redisSettings);
 
-  ascoltatori.build(settings, function(ascoltatore) {
+  ascoltatori.build(redisSettings, function(ascoltatore) {
     exports.ascoltatore = ascoltatore;
   });
+  
 };
